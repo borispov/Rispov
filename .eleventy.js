6ce25d18e4f9d5c8575f43183521360546ee21b4
Boris Povolotsky <boristofu@gmail.com>
2021-07-03 11:33:30 +0300

    fixed typo in new cmd

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,36 @@ const timeToRead = require('./lib/js/reading-time');
 const now = String(Date.now());
 
 module.exports = function(eleventyConfig) {
+
+  /*
+   * Thanks to https://github.com/robdodson
+   * From Issue:
+   * https://github.com/11ty/eleventy/issues/26
+   */
+  eleventyComputed: {
+    /**
+     * Adds support for drafts.
+     * If a page has `draft: true` in its YAML frontmatter then this snippet
+     * will set its permalink to false and exclude it from collections.
+     *
+     * For dev builds we will always render the page.
+     */
+     permalink: data => {
+      if (process.env.NODE_ENV === 'production' && data.draft) {
+        return false;
+      }
+
+      return data.permalink;
+    }
+    eleventyExcludeFromCollections: data => {
+      if (process.env.NODE_ENV === 'production' && data.draft) {
+        return true;
+      }
+
+      return false;
+    }
+  }
+
   // src folders and files
   eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' });
   eleventyConfig.addPassthroughCopy("images");
